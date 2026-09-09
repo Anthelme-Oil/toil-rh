f6c80c91415046cb4cf0b79202c78f845b69e54e
Anthelme-Oil <anthelme.kpodar@togosh.com>
2026-09-09 18:41:49 +0000

    is ready to be peared

--- app/(intranet)/admin/roles/page.tsx
+++ app/(intranet)/admin/roles/page.tsx
@@ -1087,7 +1087,7 @@ export default function AdminRolesPage() {
             <form onSubmit={handleCreateUser} className="space-y-4">
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                 <div>
-                  <label className="block text-[11px] font-bold text-slate-700 mb-1">Adresse Email (Azure AD)</label>
+                  <label className="block text-[11px] font-bold text-slate-700 mb-1">Adresse Email</label>
                   <UserEmailAutocomplete
                     value={newUser.email}
                     onChange={(val) => setNewUser({ ...newUser, email: val })}
@@ -1098,7 +1098,7 @@ export default function AdminRolesPage() {
                         name: prev.name || selectedAzureUser.displayName,
                       }));
                     }}
-                    placeholder="Saisissez pour chercher dans Azure AD..."
+                    placeholder="Saisissez pour chercher dans  AD..."
                     required
                     className="w-full px-3.5 py-2.5 bg-slate-50 border border-slate-200 rounded-xl text-xs text-slate-900 focus:bg-white focus:border-primary focus:ring-2 focus:ring-primary/20 outline-none"
                   />
